bf42d90793ba15b3ade6ec379590ddc13a2d4c40
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-03-24 15:05:41 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Header />
+  <router-view />
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&family=Paprika&display=swap");
+
+//fonts
+$paprika: "Paprika", cursive;
+$os: "Open Sans", sans-serif;
+//colors
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+  text-decoration: none;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  box-sizing: border-box;
+  font-family: $paprika;
+  line-height: 1.5;
+  font-size: 16px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <h1 class="logo__title">mi calles</h1>
+      <h2 class="logo__subtitle">the way I see the world</h2>
+    </div>
+    <nav class="nav" id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin: 1rem auto;
+  width: 80vw;
+}
+.logo {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  &__title {
+    color: blue;
+    align-self: flex-start;
+  }
+  &__subtitle {
+    align-self: flex-end;
+  }
+}
+.nav {
+  font-size: 16px;
+}
+</style>
