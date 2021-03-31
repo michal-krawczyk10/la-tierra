29da5b5c9b9df1573cac311248f3ccd5dd642803
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-04-01 01:15:19 +0200

    feat: add some set1 codes

--- src/views/Set1.vue
+++ src/views/Set1.vue
@@ -1,12 +1,31 @@
 <template>
-  <TheNavigation />
+  <div class="container">
+    <TheNavigation />
+    <section>
+      <img v-bind:src="images[0].exampleImg" />
+    </section>
+  </div>
 </template>
 <script>
 import TheNavigation from "@/components/TheNavigation";
+import { images } from "../images";
 
 export default {
   components: {
     TheNavigation,
   },
+  data() {
+    return {
+      images,
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  
+}
+
+</style>
