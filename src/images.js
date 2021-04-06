045017b899400de02eb667778272a17c7e98d853
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-04-06 17:04:03 +0200

    feat: images.js major update

--- src/images.js
+++ src/images.js
@@ -85,51 +85,205 @@ const images = [
 				imgDesc: "Iguaçu Falls",
 				imgLocal: "Brazil / Argentina",
 			},
+			{
+				imgId: 2003,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/37.jpg?raw=true",
+				imgDesc: "Paraguay River",
+				imgLocal: "Paraguay",
+			},
+			{
+				imgId: 2004,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1030247.jpg?raw=true",
+				imgDesc: "Old Man of Storr",
+				imgLocal: "Scotland",
+			},
+			{
+				imgId: 2005,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1220119.jpg?raw=true",
+				imgDesc: "Torres dle Paine",
+				imgLocal: "Chile",
+			},
+			{
+				imgId: 2006,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1230250.jpg?raw=true",
+				imgDesc: "Los Glaciares",
+				imgLocal: "Argentina",
+			},
+			{
+				imgId: 2007,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1290900.jpg?raw=true",
+				imgDesc: "Eduardo Avaroa Andean Fauna National Reserve",
+				imgLocal: "Bolivia",
+			},
+			{
+				imgId: 2008,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1380621.jpg?raw=true",
+				imgDesc: "Machu Picchu",
+				imgLocal: "Peru",
+			},
+			{
+				imgId: 2009,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1430732.jpg?raw=true",
+				imgDesc: "mediterrean clifs",
+				imgLocal: "Malta",
+			},
+			{
+				imgId: 2010,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1450335.jpg?raw=true",
+				imgDesc: "Peten Itza Lake",
+				imgLocal: "Guatemala",
+			},
+			{
+				imgId: 2011,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1460201.jpg?raw=true",
+				imgDesc: "Volcan de Fuego",
+				imgLocal: "Guatemala",
+			},
+			{
+				imgId: 2012,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1460229.jpg?raw=true",
+				imgDesc: "Volcan de Agua",
+				imgLocal: "Guatemala",
+			},
+			{
+				imgId: 2013,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/landscapes/P1030347.jpg?raw=true",
+				imgDesc: "Isle of Skye",
+				imgLocal: "Scotland",
+			},
 		],
 	},
 	{
 		setId: 3,
 		setName: "people",
-		setDesc: "test set description 3",
+		setDesc: "how do we look like",
 		exampleImg:
-			"https://raw.githubusercontent.com/michal-krawczyk10/mi-calles/master/src/assets/img/2/P1520463.jpg",
+			"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1450646.jpg?raw=true",
 		pics: [
 			{
 				imgId: 3001,
 				imgUrl:
-					"https://raw.githubusercontent.com/michal-krawczyk10/mi-calles/master/src/assets/img/2/P1520463.jpg",
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1450646.jpg?raw=true",
 				imgDesc: "test pic 3001",
-				imgLocal: "test local 3001",
+				imgLocal: "Guatemala",
 			},
 			{
 				imgId: 3002,
 				imgUrl:
-					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/2/P1520465.jpg?raw=true",
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1430547.jpg?raw=true",
 				imgDesc: "test pic 3002",
-				imgLocal: "test local 3002",
+				imgLocal: "Malta",
+			},
+			{
+				imgId: 3003,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1360112.jpg?raw=true",
+				imgDesc: "test pic 3003",
+				imgLocal: "Peru",
+			},
+			{
+				imgId: 3004,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1320007-2.jpg?raw=true",
+				imgDesc: "test pic 3004",
+				imgLocal: "Bolivia",
+			},
+			{
+				imgId: 3005,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1300718.jpg?raw=true",
+				imgDesc: "test pic 3005",
+				imgLocal: "Bolivia",
+			},
+			{
+				imgId: 3006,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/P1280241.jpg?raw=true",
+				imgDesc: "test pic 3006",
+				imgLocal: "Chile",
+			},
+			{
+				imgId: 3007,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/8.jpg?raw=true",
+				imgDesc: "test pic 3007",
+				imgLocal: "Brazil",
+			},
+			{
+				imgId: 3008,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/people/26.jpg?raw=true",
+				imgDesc: "test pic 3008",
+				imgLocal: "Paraguay",
 			},
 		],
 	},
 	{
 		setId: 4,
 		setName: "cities",
-		setDesc: "test set description 4",
+		setDesc: "places we call home",
 		exampleImg:
-			"https://raw.githubusercontent.com/michal-krawczyk10/mi-calles/master/src/assets/img/2/P1520463.jpg",
+			"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1400172.jpg?raw=true",
 		pics: [
 			{
 				imgId: 4001,
 				imgUrl:
-					"https://raw.githubusercontent.com/michal-krawczyk10/mi-calles/master/src/assets/img/2/P1520463.jpg",
-				imgDesc: "test pic 4001",
-				imgLocal: "test local 4001",
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1400172.jpg?raw=true",
+				imgDesc: "Salento",
+				imgLocal: "Colombia",
 			},
 			{
 				imgId: 4002,
 				imgUrl:
-					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/2/P1520465.jpg?raw=true",
-				imgDesc: "test pic 4002",
-				imgLocal: "test local 4002",
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1510929.jpg?raw=true",
+				imgDesc: "Korčula",
+				imgLocal: "Croatia",
+			},
+			{
+				imgId: 4003,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/050%20(2).jpg?raw=true",
+				imgDesc: "Paraty",
+				imgLocal: "Brazil",
+			},
+			{
+				imgId: 4004,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1140173%20%E2%80%94%20kopia.jpg?raw=true",
+				imgDesc: "Sao Paulo",
+				imgLocal: "Brazil",
+			},
+			{
+				imgId: 4005,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1400113.jpg?raw=true",
+				imgDesc: "Cartagena",
+				imgLocal: "Colombia",
+			},
+			{
+				imgId: 4006,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1400734.jpg?raw=true",
+				imgDesc: "Panama",
+				imgLocal: "Panama",
+			},
+			{
+				imgId: 4007,
+				imgUrl:
+					"https://github.com/michal-krawczyk10/mi-calles/blob/master/src/assets/img/cities/P1420599.jpg?raw=true",
+				imgDesc: "Kamianets-Podilskyi",
+				imgLocal: "Ukraine",
 			},
 		],
 	},
