cbf44e62fb77b9a0d9e9d3504e752fb98c19c479
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-04-08 19:35:06 +0200

    minor

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:michal-krawczyk10/la-tierra.git master:gh-pages
+git push -f git@github.com:michal-krawczyk10/latierra.git master:gh-pages
 
 cd -
